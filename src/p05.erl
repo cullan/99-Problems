2637b057bd9a3f0fd3f61cb072dcf7a5c36a3a75
Cullan Springstead <cullan.springstead@gmail.com>
2012-08-14 19:44:40 -0500

    solved problem 5.

--- src/p05.erl
+++ src/p05.erl
@@ -1,6 +1,6 @@
 -module(p05).
 -export([reverse/1]).
--include("test.erl").
+-include("test.hrl").
 
 reverse(L) ->
     reverse(L, []).
@@ -8,4 +8,15 @@ reverse(L) ->
 reverse([], Acc) ->
     Acc;
 reverse([H | T], Acc) ->
-    reverse(T, [H | Acc]);
+    reverse(T, [H | Acc]).
+
+-ifdef(TEST).
+
+p05_test_() ->
+    [{"call reverse does not crash.", ?_test(reverse([a, b, c]))},
+     {"reverse small list works.",    ?_test(?_assertEqual([c, b, a],
+                                                           reverse([a, b, c])))},
+     {"reverse empty list is empty.", ?_test(?_assertEqual([], reverse([])))}
+    ].
+
+-endif. %end tests
