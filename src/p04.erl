e7c9ac6494285b6b447f1055586a3967e08bbeef
Cullan Springstead <cullan.springstead@gmail.com>
2012-08-14 19:43:52 -0500

    add tail recursive solution to p04.

--- src/p04.erl
+++ src/p04.erl
@@ -1,12 +1,21 @@
 -module(p04).
--export([my_length/1]).
+-export([my_length/1, my_length_tr/1]).
 -include("test.hrl").
 
-%% Find the number of elements in a list
+%% Find the number of elements in a list.
 my_length([]) ->
     0;
 my_length([_X|Xs]) ->
     1 + my_length(Xs).
+
+% Tail recursive version.
+my_length_tr(L) when is_list(L) ->
+    my_length_tr(L, 0).
+
+my_length_tr([], Acc) ->
+    Acc;
+my_length_tr([_X|Xs], Acc) ->
+    my_length_tr(Xs, Acc + 1).
 
 %% Tests
 -ifdef(TEST).
@@ -16,6 +25,11 @@ p04_test_() ->
      {"my_length returns correct length",
       [?_assertEqual(0, my_length([])),
        ?_assertEqual(1, my_length([a])),
-       ?_assertEqual(2, my_length([a, b]))]}].
+       ?_assertEqual(2, my_length([a, b]))]},
+     {"call my_length_tr doesn't crash", ?_test(my_length_tr([a, b]))},
+     {"my_length_tr returns correct length",
+      [?_assertEqual(0, my_length_tr([])),
+       ?_assertEqual(1, my_length_tr([a])),
+       ?_assertEqual(2, my_length_tr([a, b]))]}].
 
 -endif. %end tests
